a73b6957827004411e34c41dc97ae57dc41c54a1
fiathux <fiathux@gmail.com>
2016-09-21 12:04:38 +0800

    add setup script for Pypi

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+        name = "whtmacro",
+        version = "0.2",
+        keywords = "documention",
+        description = "help create document use HTML template",
+        license = "GPLv2",
+        author = "Fiathux Su",
+        author_email = "fiathux@gmail.com",
+        url = "https://github.com/fiathux/whtmacro",
+        platforms = "any",
+        packages = ["whtmacro"],
+        entry_points = {
+            "console_scripts":[
+                "whtmacro = whtmacro:main"
+                ]
+            }
+        )
