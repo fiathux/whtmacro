ac4835a8b16def1bbd8b8eb69547660379439ddf
fxcat <sumoyu@droi.com>
2017-07-21 21:33:47 +0800

    complete wisp syntax design

--- whtmacro/wisp.py
+++ whtmacro/wisp.py
@@ -11,3 +11,12 @@ Fiathu Su(fiathux@gmail.com)
 from __future__ import unicode_literals
 
 from whtmacro import sexp
+
+# basic WISP object
+class WISPObject(sexp.SElement):pass
+
+class WISPExpression(sexp.SExpression, WISPObject):
+    def __init__(me, exp_gen, env, pos = sexp.SPosition(0,0,0,0)):
+        super(WISPExpression,me).__init__(exp_gen, env, pos)
+    def __call__():
+        pass
