f37bcc6b74a6cf67afe5afedab43c9d4723f6201
Su Fiathux <fiathux@>
2018-05-16 14:04:49 +0800

    try redesign markdown engine

--- whtmacro/markeng.py
+++ whtmacro/markeng.py
@@ -8,6 +8,7 @@ Fiathu Su(fiathux@gmail.com)
 2015-2018
 '''
 
+import re
 from whtmacro import htescape
 
 # HTML conetent translator
@@ -19,6 +20,31 @@ def HTMLTanslator(tag, attr, contentgen):
     else:
         return "<%s %s>%s</%s>" % (tag, attr2list(attr), "".join(contentgen), tag)
 
+# content line provider
+class contentLine(object):
+    # roll back line
+    class CLRollBack(Exception):pass
+    def __init__(me, content):
+        content = content.replace("\r\n","\n").replace("\r","\n")
+        me._lines = content.split("\n")
+        me._index = 0
+        me._withstack = []
+    def __iter__(me):
+        return me
+    def __next__(me):
+        if me._index >= len(me._lines):
+            raise StopIteration()
+        line = me._lines[me._index]
+        me._index = me._index + 1 
+        return line
+    def __enter__(me):
+        me._withstack.append(me._index)
+    def __exit__(me, exc_type, exc_val, traceback):
+        if exc_type is not None:
+            me._index = me._withstack.pop()
+        else:
+            me._withstack.pop()
+
 # basic markdown parse element
 class mdStack(object):
     LAYERS = []
@@ -26,7 +52,7 @@ class mdStack(object):
     def __init__(me, master, pcontent, nextstr):
         me._masterInstance = master
         me._parseContent = pcontent
-        
+        me._env = {}
     # next element factory
     def __iter__(me):
         def iterStack():
@@ -34,6 +60,7 @@ class mdStack(object):
             if nextContent:
                 for c in nextContent:
                     yield c
+        return iterStack()
     # output inner content
     def __str__(me):
         return me.content
@@ -61,5 +88,10 @@ class mdStack(object):
             return c(master, content, None)
         return export, None
 
-class titleLayer(mdStack):pass
+class titleLayer(mdStack):
+    LAYERS = []
+    P_SHARP = re.compile("(^|\\s*\n)(#{1,6} +([^\n]+))")
+    @classmethod
+    def parse(c, content):
+        pass
 
